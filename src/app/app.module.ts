edf62d4833c72fa28065fb3a80e30c69fbe1845c
Charity Mutembei <charity.softwaredev@gmail.com>
2023-11-04 23:36:42 +0300

    2 file(s)  edited: app.component.html app.module.ts created:  Pushed...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutUSComponent } from './about-us/about-us.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutUSComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
